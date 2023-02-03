670730b6a2ab23f68cf20683db72a9e9e31ea7ce
GabrielTrifoni <111247285+GabrielTrifoni@users.noreply.github.com>
2023-02-03 20:15:38 -0300

    Calculate streams for every map in osu folder

--- main.c
+++ main.c
@@ -1,89 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
 
-#define OSU_SONGS_DIR "C:/Users/gabri/AppData/Local/osu!/Songs/"
-#define OSU_MAP_DIR "677573 AAAA - Hoshi o Kakeru Adventure _ we are forever friends! _ [Long ver]"
-#define OSU_MAP_FILE_DIR "AAAA - Hoshi o Kakeru Adventure ~ we are forever friends! ~ [Long ver.] (Battle) [Imagined Voyage].osu"
+#define OSU_PATH "C:\\Users\\gabri\\AppData\\Local\\osu!\\Songs\\"
 #define TIMING_POINTS_LINE "[TimingPoints]"
 #define HIT_OBJECTS_LINE "[HitObjects]"
-#define BUFFER_LENGTH 255
+#define BUFFER_SIZE 255
 
-float getBPM(char buffer[BUFFER_LENGTH]);
+int print_dot_osu(char path[512]);
 
-void calcula_streams_osu_beatmap(FILE *fp, float bpm);
+void le_arquivo(char osu_dir[BUFFER_SIZE]);
 
-void calcula_um_tres_osu_beatmap(FILE *fp, float bpm);
+float get_bpm(char buffer[BUFFER_SIZE]);
 
-int get_offset(char buffer[BUFFER_LENGTH]);
+int soma_streams(FILE *fp, float bpm);
 
-int get_tipo(char buffer[BUFFER_LENGTH]);
+int get_offset(char buffer[BUFFER_SIZE]);
+
+int get_tipo(char buffer[BUFFER_SIZE]);
+
+int is_stream_map(int vet[300]);
 
 
-int main(void) {
-    FILE *fp;
-    int line_count = 0;
-    char buffer[BUFFER_LENGTH];
-    char subTiming[BUFFER_LENGTH];
-    char subHitObj[BUFFER_LENGTH];
-    float bpm = 0;
+int main() {
+    DIR *dir;
+    struct dirent *entry;
+    struct stat statbuf;
+    char path[512];
 
-    char osu_dir[255];
-    strcpy(osu_dir, OSU_SONGS_DIR);
-    strcat(osu_dir, OSU_MAP_DIR);
-    strcat(osu_dir, "/");
-    strcat(osu_dir, OSU_MAP_FILE_DIR);
-
-    if ((fp = fopen(osu_dir, "r")) == NULL) {
-        printf("Erro ao abrir arquivo.");
-        exit(0);
+    strcpy(path, OSU_PATH);
+    dir = opendir(path);
+    if (dir == NULL) {
+        perror("opendir");
+        return 1;
     }
 
-    while (fgets(buffer, BUFFER_LENGTH, fp)) {
+    while ((entry = readdir(dir)) != NULL) {
+        strcpy(path + strlen(path), entry->d_name);
+        strcat(path, "\\");
+        stat(path, &statbuf);
+        if (S_ISDIR(statbuf.st_mode)) {
+            print_dot_osu(path);
+        }
+        path[strlen(path) - strlen(entry->d_name) - 1] = '\0';
+    }
+
+    closedir(dir);
+    return 0;
+}
+
+int print_dot_osu(char path[512]) {
+    DIR *dir;
+    struct dirent *entry;
+    char path_copy[512];
+
+    dir = opendir(path);
+    if (dir == NULL) {
+        perror("opendir");
+        return 1;
+    }
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (strstr(entry->d_name, ".osu") != NULL) {
+            strcpy(path_copy, path);
+            strcat(path_copy, entry->d_name);
+            le_arquivo(path_copy);
+        }
+    }
+
+    closedir(dir);
+    return 0;
+}
+
+void le_arquivo(char osu_dir[BUFFER_SIZE]) {
+    FILE *fp;
+    char buffer[BUFFER_SIZE];
+    char subTiming[BUFFER_SIZE];
+    char subHitObj[BUFFER_SIZE];
+    float bpm = 0;
+    char map_name[BUFFER_SIZE];
+
+    if ((fp = fopen(osu_dir, "r")) == NULL) {
+        printf("Erro ao abrir arquivo %s", osu_dir);
+        return;
+    }
+
+    while (fgets(buffer, BUFFER_SIZE, fp)) {
         memcpy(subTiming, &buffer[0], 14);
         subTiming[14] = '\0';
 
         memcpy(subHitObj, &buffer[0], 14);
         subHitObj[12] = '\0';
 
-        if (strcmp(subTiming, TIMING_POINTS_LINE) == 0) { // chegou na linha de timing points
-            // printf("line[%d]: %s", line_count, buffer);
-            fgets(buffer, BUFFER_LENGTH, fp); // le o primeiro timing point
-            printf("BPM: %.5f\n", 60000 / getBPM(buffer));
-            printf("OFFSET DIFF (1/1): %.5f\n", getBPM(buffer));
-            printf("OFFSET DIFF STREAM: %.5f\n", getBPM(buffer) / 4);
-            bpm = getBPM(buffer);
-            line_count++;
-            // printf("line[%d]: %s", line_count, buffer);
-            memcpy(subTiming, &buffer[0], 14);
-            subTiming[14] = '\0';
+        if (strcmp(subTiming, TIMING_POINTS_LINE) == 0) {
+            fgets(buffer, BUFFER_SIZE, fp);
+            bpm = get_bpm(buffer);
+            //printf("\n\nBPM: %.5f\n", 60000 / bpm);
         }
 
-        if (strcmp(subHitObj, HIT_OBJECTS_LINE) == 0) { // chegou na linha de hit objects
-            // printf("line[%d]: %s", line_count, buffer);
-            /*fgets(buffer, BUFFER_LENGTH, fp); // le o primeiro hit object
-            line_count++;
-            printf("line[%d]: %s", line_count, buffer);
-            memcpy(subHitObj, &buffer[0], 14);
-            subHitObj[12] = '\0';*/
-
-            calcula_streams_osu_beatmap(fp, bpm);
+        if (strcmp(subHitObj, HIT_OBJECTS_LINE) == 0) {
+            if (soma_streams(fp, bpm) == 1) {
+                strcpy(map_name, &osu_dir[40]);
+                printf("%s\n\n", map_name);
+            }
         }
-
-        line_count++;
     }
 
     fclose(fp);
 }
 
-float getBPM(char buffer[BUFFER_LENGTH]) {
+float get_bpm(char buffer[BUFFER_SIZE]) {
     float bpm;
     char buffer_bpm[255], ch;
     int cont = 0, size = 0;
-    // printf("buffer: %s len: %llu\n", buffer, strlen(buffer));
 
     for (int i = 0; i < strlen(buffer); i++) {
-        // printf("ch[%d]: %c\n", i, ch);
         ch = buffer[i];
 
         if (ch == ',') {
@@ -100,23 +133,17 @@ float getBPM(char buffer[BUFFER_LENGTH]) {
 
     buffer_bpm[size] = '\0';
 
-    // printf("buffer_bpm: %s\n", buffer_bpm);
-    // printf("buffer_bpm (int): %.14f\n", atof(buffer_bpm));
-
     bpm = (float) atof(buffer_bpm);
-    // printf("bpm: %.5f\n", bpm);
 
     return bpm;
 }
 
-int get_offset(char buffer[BUFFER_LENGTH]) {
+int get_offset(char buffer[BUFFER_SIZE]) {
     int offset;
     char buffer_offset[255], ch;
     int cont = 0, size = 0;
-    // printf("buffer: %s len: %llu\n", buffer, strlen(buffer));
 
     for (int i = 0; i < strlen(buffer); i++) {
-        // printf("ch[%d]: %c\n", i, ch);
         ch = buffer[i];
 
         if (ch == ',') {
@@ -133,123 +160,16 @@ int get_offset(char buffer[BUFFER_LENGTH]) {
 
     buffer_offset[size] = '\0';
 
-    // printf("buffer_offset: %s\n", buffer_offset);
-    // printf("buffer_offset (int): %.14f\n", atof(buffer_offset));
-
     offset = atoi(buffer_offset);
-    // printf("bpm: %.5f\n", bpm);
 
     return offset;
 }
 
-void calcula_streams_osu_beatmap(FILE *fp, float bpm) {
-    char buffer[BUFFER_LENGTH];
-    int cont = 0, total = 0, tipo, offset_hitobject, offset_hitobject_antes, diff_objs, diff_stream_esperado =
-            (int) bpm / 4;
-    int vet[33];
-
-    for (int i = 0; i < 33; ++i) {
-        vet[i] = 0;
-    }
-
-    printf("diff_stream_esperado: %d\n", diff_stream_esperado);
-
-    fgets(buffer, BUFFER_LENGTH, fp);
-    offset_hitobject_antes = get_offset(buffer);
-
-    while (fgets(buffer, BUFFER_LENGTH, fp)) {
-        // printf("buffer: %s\n", buffer);
-        offset_hitobject = get_offset(buffer);
-        tipo = get_tipo(buffer);
-        //printf("offset_antes: %d   ###   offset: %d   ###   diff: %d\n", offset_hitobject_antes, offset_hitobject, offset_hitobject - offset_hitobject_antes);
-
-        diff_objs = offset_hitobject - offset_hitobject_antes;
-
-        if (diff_objs + 2 >= diff_stream_esperado && diff_objs - 2 <= diff_stream_esperado && tipo != 12) { // é stream
-            cont++;
-        } else {
-            if (cont >= 32)
-                vet[32]++;
-            else
-                vet[cont]++;
-            cont = 0;
-        }
-        offset_hitobject_antes = offset_hitobject;
-
-    }
-
-    printf("\n---------------------\nVetor: \n");
-
-    for (int i = 0; i < 33; ++i) {
-        if (vet[i] != 0)
-            printf("%d: %d\n", i + 1, vet[i]);
-    }
-
-    for (int i = 0; i < 33; ++i) {
-        total += vet[i];
-    }
-
-    printf("Total: %d", total);
-}
-
-
-void calcula_um_tres_osu_beatmap(FILE *fp, float bpm) {
-    char buffer[BUFFER_LENGTH];
-    int cont = 0, total = 0, tipo, offset_hitobject, offset_hitobject_antes, diff_objs, diff_stream_esperado =
-            (int) bpm / 3;
-    int vet[33];
-
-    for (int i = 0; i < 33; ++i) {
-        vet[i] = 0;
-    }
-
-    printf("diff_stream_esperado: %d\n", diff_stream_esperado);
-
-    fgets(buffer, BUFFER_LENGTH, fp);
-    offset_hitobject_antes = get_offset(buffer);
-
-    while (fgets(buffer, BUFFER_LENGTH, fp)) {
-        // printf("buffer: %s\n", buffer);
-        offset_hitobject = get_offset(buffer);
-        tipo = get_tipo(buffer);
-        //printf("offset_antes: %d   ###   offset: %d   ###   diff: %d\n", offset_hitobject_antes, offset_hitobject, offset_hitobject - offset_hitobject_antes);
-
-        diff_objs = offset_hitobject - offset_hitobject_antes;
-
-        if (diff_objs + 2 >= diff_stream_esperado && diff_objs - 2 <= diff_stream_esperado && tipo != 12) { // é stream
-            cont++;
-        } else {
-            if (cont >= 32)
-                vet[32]++;
-            else
-                vet[cont]++;
-            cont = 0;
-        }
-        offset_hitobject_antes = offset_hitobject;
-
-    }
-
-    printf("\n---------------------\nVetor: \n");
-
-    for (int i = 0; i < 33; ++i) {
-        if (vet[i] != 0)
-            printf("%d: %d\n", i + 1, vet[i]);
-    }
-
-    for (int i = 0; i < 33; ++i) {
-        total += vet[i];
-    }
-
-    printf("Total: %d", total);
-}
-
-
-int get_tipo(char buffer[BUFFER_LENGTH]) {
+int get_tipo(char buffer[BUFFER_SIZE]) {
     int tipo, cont = 0, size = 0;
     char ch, buffer_tipo[5];
 
     for (int i = 0; i < strlen(buffer); i++) {
-        // printf("ch[%d]: %c\n", i, ch);
         ch = buffer[i];
 
         if (ch == ',') {
@@ -264,10 +184,74 @@ int get_tipo(char buffer[BUFFER_LENGTH]) {
             break;
     }
 
-
     buffer_tipo[size] = '\0';
 
     tipo = atoi(buffer_tipo);
 
     return tipo;
 }
+
+int soma_streams(FILE *fp, float bpm) {
+    char buffer[BUFFER_SIZE];
+    int cont = 0, tipo, tamanho_max_stream = 300;
+    int offset_hit_object, offset_hit_object_antes, diferenca_hit_objects, diferenca_esperada = (int) (bpm / 4 + 0.5f);
+    int vet[tamanho_max_stream];
+
+    memset(vet, 0, sizeof(vet));
+
+    if (fgets(buffer, BUFFER_SIZE, fp)) {
+        offset_hit_object_antes = get_offset(buffer);
+    } else {
+        return 727;
+    }
+
+    while (fgets(buffer, BUFFER_SIZE, fp)) {
+        offset_hit_object = get_offset(buffer);
+        tipo = get_tipo(buffer);
+
+        diferenca_hit_objects = offset_hit_object - offset_hit_object_antes;
+
+        if (abs(diferenca_hit_objects - diferenca_esperada) <= 2 && tipo != 12) {
+            cont++;
+        } else {
+            vet[cont >= tamanho_max_stream ? tamanho_max_stream - 1 : cont]++;
+            cont = 0;
+        }
+        offset_hit_object_antes = offset_hit_object;
+    }
+
+    vet[cont >= tamanho_max_stream ? tamanho_max_stream - 1 : cont]++;
+
+    if (is_stream_map(vet) == 1) {
+        return 1;
+    }
+
+    return 0;
+}
+
+/*
+ * condition to be a stream map: more than 15 bursts with 5 or more notes each
+ */
+int is_stream_map(int vet[300]) {
+    int sum = 0;
+
+    for (int i = 5; i < 300; i++) {
+        sum += vet[i];
+    }
+
+    if (sum >= 15) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+/*
+ * maps with doubles
+int is_stream_map(int vet[300]) {
+    if (vet[2] >= 30) {
+        return 1;
+    } else {
+        return 0;
+    }
+}*/
